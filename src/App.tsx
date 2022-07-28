7e50706716e2bb45431dc32836e52d6cee628142
Tidbits JS <tidbitsjs@gmail.com>
2022-07-28 20:21:54 +0530

    app setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-josefinSans font-semibold text-9xl">
+        Far far away, behind the word mountains
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff4c60",
+        secondary: "#65ebe7",
+        titlesColor: "#454360",
+        blackColor: "#4b4870",
+        textColor: "#596172",
+        borderColor: "#eee",
+        bgColor: "#f9f9fe",
+      },
+      fontFamily: {
+        serifPro: ["Source Serif Pro", "serif"],
+        josefinSans: ["Josefin Sans", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};

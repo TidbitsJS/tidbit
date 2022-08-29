6f409b51c7880af31cb046c94ba096f9daa484a4
Tidbits JS <tidbitsjs@gmail.com>
2022-08-29 23:21:14 +0530

    sticky sidebar

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
             <Card />
           </div>
 
-          <div className="flex flex-[.95] flex-col ml-16 static top-0">
+          <div className="flex flex-[.95] flex-col ml-16 sticky top-0 h-fit">
             <div className="my-6 flex flex-col">
               <div className="flex flex-row items-center">
                 <h4 className="font-josefinSans font-bold text-titlesColor text-[24px] tracking-[.25px]">
